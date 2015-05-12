4a4ad1276556a7f730399a61bc30b6daa6df1c0b
Stefan Doychev <sdoychev@gmail.com>
2015-05-12 14:10:42 +0300

    Establish connection

--- app/src/main/java/com/smd/studio/carcontroller/MainActivity.java
+++ app/src/main/java/com/smd/studio/carcontroller/MainActivity.java
@@ -1,9 +1,17 @@
 package com.smd.studio.carcontroller;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.jcraft.jsch.ChannelExec;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.JSchException;
+import com.jcraft.jsch.Session;
+
+import java.io.ByteArrayOutputStream;
+import java.util.Properties;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +20,37 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // CREATE CONNECTION AND EXECUTE COMMAND --------------------
+        JSch jsch = new JSch();
+        Session session = null;
+        try {
+            session = jsch.getSession("pi", "192.168.1.103", 22);
+
+            session.setPassword("raspberry");
+
+            // Avoid asking for key confirmation
+            Properties prop = new Properties();
+            prop.put("StrictHostKeyChecking", "no");
+            session.setConfig(prop);
+
+            session.connect();
+
+            // SSH Channel
+            ChannelExec channelssh = (ChannelExec) session.openChannel("exec");
+
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            channelssh.setOutputStream(baos);
+
+            // Execute command
+            channelssh.setCommand("ls");
+            channelssh.connect();
+            channelssh.disconnect();
+        } catch (JSchException e) {
+            e.printStackTrace();
+        }
+        //--------------------
+
     }
 
 
