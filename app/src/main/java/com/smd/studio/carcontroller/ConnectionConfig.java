50fbd7eff26a1ea9667eb92e300e396902dad60c
Stefan Doychev <sdoychev@gmail.com>
2015-05-22 23:33:44 +0300

    Refactor. ConnectionConfig now keeps track for the host address.

---
+++ app/src/main/java/com/smd/studio/carcontroller/ConnectionConfig.java
@@ -0,0 +1,76 @@
+package com.smd.studio.carcontroller;
+
+import android.app.Application;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.InetAddress;
+
+/**
+ * Created by Doychev on 22.5.2015 ã..
+ */
+
+public class ConnectionConfig extends Application {
+
+    private static ConnectionConfig mInstance;
+    private String gatewayAddress;
+    private String hostAddress;
+
+    public static synchronized ConnectionConfig getInstance() {
+        return mInstance;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mInstance = this;
+        gatewayAddress = "192.168.1.1";
+        hostAddress = "192.168.1.103";
+        obtainHostIpAddress();
+    }
+
+    public String getGatewayAddress() {
+        return gatewayAddress;
+    }
+
+    public void setGatewayAddress(String address) {
+        gatewayAddress = address;
+    }
+
+    public String getHostAddress() {
+        return hostAddress;
+    }
+
+    public void setHostAddress(String address) {
+        hostAddress = address;
+    }
+
+    public void obtainHostIpAddress() {
+        BufferedReader br = null;
+        boolean isFirstLine = true;
+        try {
+            br = new BufferedReader(new FileReader("/proc/net/arp"));
+            String line;
+            while ((line = br.readLine()) != null) {
+                if (isFirstLine) {
+                    isFirstLine = false;
+                    continue;
+                }
+                String[] splitted = line.split(" +");
+                if (splitted != null && splitted.length >= 4 && InetAddress.getByName(splitted[0]).isReachable(500) && !splitted[0].equals(ConnectionConfig.getInstance().getGatewayAddress())) {
+                    setHostAddress(splitted[0]);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                br.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
--- app/src/main/java/com/smd/studio/carcontroller/RCTask.java
+++ app/src/main/java/com/smd/studio/carcontroller/RCTask.java
@@ -7,9 +7,6 @@ import com.jcraft.jsch.JSch;
 import com.jcraft.jsch.JSchException;
 import com.jcraft.jsch.Session;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.Properties;
 
 /**
@@ -17,14 +14,11 @@ import java.util.Properties;
  */
 public class RCTask extends AsyncTask<String, Void, Void> {
 
-    private String hostAddress = "192.168.1.103";
-
     @Override
     protected Void doInBackground(String... params) {
         try {
-            getListOfConnectedDevice();
             JSch jsch = new JSch();
-            Session session = jsch.getSession(Constants.USERNAME, hostAddress, 22);
+            Session session = jsch.getSession(Constants.USERNAME, ConnectionConfig.getInstance().getHostAddress(), 22);
             session.setPassword(Constants.PASSWORD);
             // Avoid asking for key confirmation
             Properties prop = new Properties();
@@ -40,27 +34,5 @@ public class RCTask extends AsyncTask<String, Void, Void> {
             e.printStackTrace();
         }
         return null;
-    }
-
-    public void getListOfConnectedDevice() {
-        BufferedReader br = null;
-        try {
-            br = new BufferedReader(new FileReader("/proc/net/arp"));
-            String line;
-            while ((line = br.readLine()) != null) {
-                String[] splitted = line.split(" +");
-                if (splitted != null && splitted.length >= 4) {
-                    hostAddress = splitted[0];
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                br.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
     }
 }
